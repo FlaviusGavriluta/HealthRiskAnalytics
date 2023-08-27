c8fdd34dd24f29d2d95ca00ee941cff240764c79
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-08-27 23:53:57 +0300

    implement calculateAge method

--- src/main/java/com/codecool/healthriskanalytics/service/AnalyticsService.java
+++ src/main/java/com/codecool/healthriskanalytics/service/AnalyticsService.java
@@ -3,12 +3,26 @@ package com.codecool.healthriskanalytics.service;
 import com.codecool.healthriskanalytics.model.Person;
 import com.codecool.healthriskanalytics.model.WeightCondition;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
 public class AnalyticsService {
     private static final double OVERWEIGHT_BMI_LIMIT = 25.0;
     private static final int OVERWEIGHT_YEARS_LIMIT = 3;
 
     public int calculateAge(Person person) {
-        return 0;
+        // Parse the birth date string to a LocalDate object
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate birthDate = LocalDate.parse(person.birthDate() , formatter);
+
+        // Get the current date
+        LocalDate today = LocalDate.now();
+
+        // Calculate the age
+        long age = ChronoUnit.YEARS.between(birthDate, today);
+
+        return (int) age;
     }
 
     public double[] calculateBmiSeries(Person person) {
