7a2c0e32880e8c7a706c7b435527e24344eaebd5
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-08-30 18:07:35 +0300

    test all the methods in main class

--- src/main/java/com/codecool/healthriskanalytics/Application.java
+++ src/main/java/com/codecool/healthriskanalytics/Application.java
@@ -5,6 +5,8 @@ import com.codecool.healthriskanalytics.service.AnalyticsService;
 import com.codecool.healthriskanalytics.service.AnalyticsServiceImpl;
 import com.codecool.healthriskanalytics.service.PersonProvider;
 
+import java.util.Arrays;
+
 public class Application {
 
     public static void main(String[] args) {
@@ -13,5 +15,13 @@ public class Application {
 
         AnalyticsService analyticsService = new AnalyticsServiceImpl();
 
+        // Testează calculateBmiSeries pentru prima persoană
+        double[] bmiSeries = analyticsService.calculateBmiSeries(persons[1]);
+        System.out.println("BMI for the person number " + persons[1].id() + ": " + Arrays.toString(bmiSeries));
+        System.out.print("Person number " + persons[1].id() + " is " + analyticsService.calculateAge(persons[1]) + " years old ");
+        System.out.println("and is " + analyticsService.determineWeightCondition(persons[1]));
+        System.out.println("_______________________________________________________________");
+
+        System.out.println("Overweight risk ratio: " + analyticsService.calculateOrr(persons));
     }
 }
